644c9bf3d7251120685c47e56977ff74de876212
Choscura <choscura@choscura.com>
2016-10-02 21:01:36 -0700

    Updating classes.py to linux and PC version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # UNUI
-You Knew it.
+Camera control / graphics processing / environment abstractions
+
+This is a test project given by IUNU (the project title is that name spelled backwards) to test my coding skills.  Since I'm wrapping this up and it's something that seems interesting and like it could have practical applications, whether I get this job or not, I'm putting some thought into it and making it usable by myself, or possibly others, later on, as a graphics workhorse package for a set of functionality useful for building digital abstractions of real physical environments with actors <entities with behavior>, some event handling, and some physical geometry calculation abilities- nothing too heavy, but figuring out distances between points and so on.  {idea for later- google earth or google maps integration?}
+
+Paradigms: IUNU was founded as a digital plant-growing light company, to produce and market a very cool digitally spectrum-controlled plasma light that changes the color of the light depending on what the plant it is growing needs- for example, marijuana growers need more blue-shifted light for the early part of the growing season and a more red-shifted light for later in the season.  As part of that, they have camera sensors that detect the plants to figure out the correct light spectrum to shine and so on, and these cameras are starting to be used more on their own to track the growth of individual plants, down to the individual leaves. It's very cool, this is a test to see how I approach that. 
+
+	there's a camera mounted on a pair of servos. For purposes of this exercise, those servos each have a pair of external functions, one for setting its rotation, one for reading its rotation. Given the position of the camera as locally defined constant as one element in an array of the positions of all the cameras in the room, and a (X,Y,Z) target coordinate, determine if this camera is the closest on any side of the target, and if so, aim at the target and at a synchronized time, take a picture.
+
+ proper python project setup, tests, with gitbhub repo and commits that make sense.
+
+ Asking questions is encouraged. Finding hidden requirements is half the battle of building software. Getting software working, then refactoring is the other. Show me how you think through a problem.
--- classes.py
+++ classes.py
@@ -1,56 +1,73 @@
 """
 	IUNU Test project.
 
-	Kendall,
-
-Lets try a little coding test under real world conditions. For the record, your code is yours until we hire you or buy/license it etc.
-
-Ive got a camera mounted on a pair of servos. For purposes of this exercise, those servos each have a pair of external functions, one for setting its rotation, one for reading its rotation. Given the position of the camera as locally defined constant as one element in an array of the positions of all the cameras in the room, and a (X,Y,Z) target coordinate, determine if this camera is the closest on any side of the target, and if so, aim at the target and at a synchronized time, take a picture.
-
-I'm looking for a proper python project setup, including tests, with gitbhub repo and commits that make sense. Asking questions is encouraged. Finding hidden requirements is half the battle of building software. Getting software working, then refactoring is the other. Show me how you think through a problem.
-
 """
-import unittest
 from math import sqrt
 
+class plant:
+    """
+    a data member to represent the plant
+    """
+
+class actor:
+    """
+    Things that have behavior.
+    """
+
 class camera:
+    """     
+    The camera class represents the physical cameras in the abstracted greenhouse, 
+    for the purposes of calculating and controlling their behavior
+    """
 	#Position of camera origin point in space
-	camerax=(float)
-	cameray=(float)
-	cameraz=(float)
+    camerax=(float)
+    cameray=(float)
+    cameraz=(float)
 
+    #init function to handle class construction
+    def __init__(self, x, y, z):
+        self.camerax = x
+        self.cameray = y
+        self.cameraz = z
 
-	#init function to handle class construction
-	def __init__(self, x, y, z):
-		self.camerax = x
-		self.cameray = y
-		self.cameraz = z
+    #a function to find the distance between the camera and plant in 3D space
+    def DistanceToPlant(self, plantx, planty, plantz):
+        """
+        Very simply, think of a cube- we're measuring a straight line from 
+        top back left to bottom right front, or whatever. So we calculate 
+        the hypotenuse for the bottom, then we use that hypotenuse squared
+        to calculate the hypotenuse of the triangle remaining.
+        """
+        #Horizontal first:
+        HorizontalX = (self.camerax-plantx)
+        HorizontalY = (self.cameray-planty)
+        HorizontalHypotenuse=float(sqrt((HorizontalX**2)+(HorizontalY**2)))
+        #Now for the vertical part:
+        VerticalLeg=(self.cameraz-plantz)
+        VerticalHypotenuse=float(sqrt((VerticalLeg**2)+(HorizontalHypotenuse**2)))
+	#If it all worked, this should be the distance. 
+        return VerticalHypotenuse
+    
+    def TakePicture(self):
+        """
+        a black box to take pictures
+        """
+        print("Click!")
 
-
-	#a function to find the distance between the camera and plant in 3D space
-	def DistanceToPlant(self, plantx, planty, plantz):
-		#Very simply, think of a cube- we're measuring a straight line from top back left to bottom right front, or whatever. So we calculate the hypotenuse for the bottom, then we use that hypotenuse squared to calculate the hypotenuse of the triangle remaining.
-		
-		#Horizontal first:
-		HorizontalX 			= 	(self.camerax-plantx)
-		HorizontalY 			= 	(self.cameray-planty)
-		HorizontalHypotenuse	=	float(sqrt((HorizontalX**2)+(HorizontalY**2)))
-		
-		#Now for the vertical part:
-		VerticalLeg 			=	(self.cameraz-plantz)
-		VerticalHypotenuse		=	float(sqrt((VerticalLeg**2)+(HorizontalHypotenuse**2)))
-		#If it all worked, this should be the distance. 
-		return VerticalHypotenuse
-
-
-	#To black box the actual handling of picture tacking for the moment
-	def TakePicture(self):
-		print("Click!")
-
-#the main function to actually control program flow and test things as I write
 def main():
-	Derp = camera(1,2,3)
-	print(Derp.DistanceToPlant(4,5,6))
-	Derp.TakePicture()
+    """
+    Main function, for testing
+    """
+    Derp = camera(1,2,3)
+    print(Derp.DistanceToPlant(4,5,6))
+    Derp.TakePicture()
 
 main()
+
+"""
+There is already a separate test document. The following code is being 
+generated to populate that, which will deal with package tests.
+"""
+
+import unittest
+
