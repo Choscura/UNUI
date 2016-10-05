65ff563033cd4c87e8a426b93cf11dd903052823
Choscura <choscura@choscura.com>
2016-10-05 00:33:44 -0700

    end of day commit,

--- classes.py
+++ classes.py
@@ -1,15 +1,12 @@
 """
-
     UNUI
-    IUNU environment abstraction toolkit 
-
+    IUNU environment abstraction toolkit.  See README.md for documentation.
 """
 from math import sqrt
 
-
 class actor:
     """
-    An "actor" in UNUI space is a noun that has <or can possibly have> 
+    An actor in UNUI space is a noun that has <or can possibly have> 
     behavior. A light is an actor, a camera is an actor, a plant is an actor.
     """
     #Spatial positioning
@@ -44,26 +41,8 @@ class actor:
         self.actorx = x
         self.actory = y
         self.actorz = z
-
-class plant(actor):
-    """
-    a data member to represent the plant
-    """
-
-class camera(actor):
-    """     
-    The camera class represents the physical cameras in the abstracted greenhouse, 
-    for the purposes of calculating and controlling their behavior
-    """
-
-    #init function to handle class construction
-    def __init__(self, x, y, z):
-        self.camerax = x
-        self.cameray = y
-        self.cameraz = z
-
-    #a function to find the distance between the camera and plant in 3D space
-    def DistanceToPlant(self, plantx, planty, plantz):
+    
+    def getdistance(self, measured_x, measured_y, measured_z):
         """
         Very simply, think of a cube- we're measuring a straight line from 
         top back left to bottom right front, or whatever. So we calculate 
@@ -71,37 +50,42 @@ class camera(actor):
         to calculate the hypotenuse of the triangle remaining.
         """
         #Horizontal first:
-        HorizontalX = (self.camerax-plantx)
-        HorizontalY = (self.cameray-planty)
+        HorizontalX = (self.camerax-actorx)
+        HorizontalY = (self.cameray-actory)
         HorizontalHypotenuse=float(sqrt((HorizontalX**2)+(HorizontalY**2)))
         #Now for the vertical part:
-        VerticalLeg=(self.cameraz-plantz)
+        VerticalLeg=(self.cameraz-actorz)
         VerticalHypotenuse=float(sqrt((VerticalLeg**2)+(HorizontalHypotenuse**2)))
 	#If it all worked, this should be the distance. 
         return VerticalHypotenuse
+
+class plant(actor):
+    """
+    a data member to represent the plant
+    """
     
+class camera(actor):
+    """     
+    The camera class represents the physical cameras in the abstracted greenhouse, 
+    """
+
+    def __init__(self, x, y, z):
+        """
+        camera constructors
+        """
+        self.camerax = x
+        self.cameray = y
+        self.cameraz = z
+
     def TakePicture(self):
         """
-        a black box to take pictures
+        placeholder for picam handling code 
         """
-        print("Click!")
 
 def main():
     """
     Main function, for testing
     """
     
-    #Derp = camera(1,2,3)
-    #print(Derp.DistanceToPlant(4,5,6))
-    #Derp.TakePicture()
-
 main()
 
-"""
-There is already a separate test document. The following code is being 
-generated to populate that, which will deal with package tests.
-"""
-
-import unittest
-
-print("classes.py ran")
